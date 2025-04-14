f26e41aad9d7b47875d348f53bdee1f6a5c89bdb
buha <bukhantcev@mail.ru>
2025-04-15 00:11:38 +0300

    14.04.25_room

--- room/views.py
+++ room/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, Group
+from django.contrib.auth import update_session_auth_hash
+from django.contrib import messages
 from accounts.forms import FormValid
 
 from accounts.models import Profile
@@ -15,8 +17,50 @@ def index(request):#--------------------------------------------ROOM
         if not user.groups.filter(name='podtvergdennie').exists():
             users_list.append(user)
 
+    podrazdelenie_list = Podrazdelenie.objects.all()
+    dolgnost_list = Dolgnost.objects.all()
+
+    if request.method == 'POST':
+        profile = Profile.objects.get(user=request.user)
+
+        # Обновление профиля
+        request.user.first_name = request.POST.get('first_name')
+        request.user.last_name = request.POST.get('last_name')
+        request.user.save()
+
+        profile.first_name = request.POST.get('first_name')
+        profile.last_name = request.POST.get('last_name')
+        profile.phone = request.POST.get('phone')
+        profile.podrazdelenie = Podrazdelenie.objects.get(id=request.POST.get('podrazdelenie'))
+        profile.dolgnost = Dolgnost.objects.get(id=request.POST.get('dolgnost'))
+        profile.save()
+
+        # Смена пароля, только если введены соответствующие поля
+        if request.POST.get('old_password') or request.POST.get('new_password1') or request.POST.get('new_password2'):
+            old_password = request.POST.get('old_password')
+            new_password1 = request.POST.get('new_password1')
+            new_password2 = request.POST.get('new_password2')
+
+            if not old_password or not new_password1 or not new_password2:
+                messages.error(request, 'Пожалуйста, заполните все поля для смены пароля.')
+            elif new_password1 != new_password2:
+                messages.error(request, 'Новые пароли не совпадают.')
+            elif not request.user.check_password(old_password):
+                messages.error(request, 'Старый пароль введён неверно.')
+            else:
+                request.user.set_password(new_password1)
+                request.user.save()
+                update_session_auth_hash(request, request.user)
+                messages.success(request, 'Пароль успешно изменён.')
+        else:
+            messages.success(request, 'Данные профиля сохранены.')
+
+        return redirect('room')
+
     context = {
-        'users': users_list
+        'users': users_list,
+        'podrazdelenie_list': podrazdelenie_list,
+        'dolgnost_list': dolgnost_list
     }
     return render(request, 'room/index.html', context=context)
 
@@ -52,7 +96,6 @@ def form_valid(request):
 
             user = User.objects.get(username=Profile.objects.get(id=request.POST.get('delete')).user)
             user.delete()
-
 
             return redirect('/room')
     context = {
